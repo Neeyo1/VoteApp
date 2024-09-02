ed914cb8b5d72fb70d06925811169a942343366d
Neeyo <jakub.nycz99@gmail.com>
2024-09-02 14:51:13 +0200

    Initial commit

---
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,20 @@
+using System.Security.Claims;
+
+namespace API.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string GetUsername(this ClaimsPrincipal user)
+    {
+        var username = user.FindFirstValue(ClaimTypes.Name) 
+            ?? throw new Exception("No username found in token");
+        return username;
+    }
+
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier) 
+            ?? throw new Exception("No username found in token"));
+        return userId;
+    }
+}
